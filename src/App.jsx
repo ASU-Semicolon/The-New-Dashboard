132bae91c474a292052178b339213738e01d1ac1
HanaTarek <105328136+HanaTarek@users.noreply.github.com>
2024-02-14 23:55:26 +0200

    Add Button component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Button from './Components/Button';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -28,6 +29,10 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Button styleProp="primary" >Button</Button>
+      <Button styleProp="secondary" >Button</Button>
+      <Button styleProp="transparent" >button</Button>
+      <Button styleProp="Redtransparent" >button</Button>
     </div>
   )
 }
