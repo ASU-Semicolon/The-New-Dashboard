ecdb4aae57cfd7988fe2cc0358975d4e99367aba
HagerRagaee <hagerragaee9@gmail.com>
2024-02-15 14:05:42 -0800

    input field with changes

--- src/components/InputWithLabel.jsx
+++ src/components/InputWithLabel.jsx
@@ -1,22 +1,17 @@
 import "./inputWithLabel.css"
-function InputWithLabel(props) {
-    const type = props.Type;
-    if (type == "input") {
+function InputWithLabel({ Labelvalue, placeholder, multiline }) {
+    if (!multiline) {
         return (
             <div className="input">
-                <label className="label" >{props.Labelvalue}</label>
-                <form >
-                    <input type="text" placeholder={"Enter " + props.Labelvalue}/>
-                </form>
+                <label className="label">{Labelvalue}</label>
+                <input type="text" placeholder={placeholder}/>
             </div>
         );
-    }
-     else {
+    } else {
         return (
             <div className="input">
-                <label className="label">{props.Labelvalue}</label>
-                <textarea placeholder={"Enter " + props.Labelvalue}  />
-                
+                <label className="label">{Labelvalue}</label>
+                <textarea placeholder={placeholder} />
             </div>
         );
     }
