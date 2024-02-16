69365ea4396be80dfe749fb230bdc326a46cca23
Ahmed Khaled <80486237+Ahmed-Khaled24@users.noreply.github.com>
2024-02-16 13:19:50 +0200

    feat: update file structure
    create folder for each component using naming
    convention of {component-name}.component.jsx
    and {component-name}.style.css.
    install prettier for code formatting and add
    prettier configuration file.

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,8 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-//import for testing
-// import Modal from './components/Modal';
-
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-  // Modal state
-  const [showModal, setShowModal] = useState(false);
-
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      {/* Buttons for testing */}
-      {/* <button
-        onClick={() => {
-          setShowModal(true);
-        }}
-      >
-        Show Modal
-      </button>
-      <Modal setShowModal={setShowModal} showModal={showModal}>
-        <button
-          onClick={() => {
-            setShowModal(false);
-          }}
-        >
-          hide
-        </button>
-      </Modal> */}
-    </div>
-  );
+    return <></>;
 }
 
 export default App;
