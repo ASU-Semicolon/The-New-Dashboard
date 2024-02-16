b06849881e6d10759a123d3ad81d095a26a79626
Hajar <1900640@eng.asu.edu.eg>
2024-02-16 21:10:50 +0200

    feat: handlechange-function which keeps track the change in the input of the search-bar Search-Bar featurer/handlechange-function
    perf: creating a state to add the class name conditionally instead of using DOM Search-Bar perf/isEntered-state

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import { useState } from "react";
 import SearchBar from "./components/search-bar/search.component.jsx";
 
 function App() {  
+    const [searchinput,setSearchInput]= useState("");
 return (
     <div className='App'>
-        <SearchBar />
+        <SearchBar  
+        setSearchInput={setSearchInput} 
+        searchinput={searchinput}/>
     </div>
 );
 }
