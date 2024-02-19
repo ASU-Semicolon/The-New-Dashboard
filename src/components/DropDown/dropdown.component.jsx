88232812d73c4d5df824633bd58c85b057e72024
ahmedglala <102909097+ahmedglala@users.noreply.github.com>
2024-02-19 20:34:02 +0200

    dropdown menu updated
    changing the color to #A4A4A4
    
    and changing google icons to react icons

--- src/components/DropDown/dropdown.component.jsx
+++ src/components/DropDown/dropdown.component.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useRef } from "react";
 import "./dropdown.style.css";
+import { MdOutlineExpandMore } from "react-icons/md";
 
 const Dropdown = ({ options, onSelect }) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -31,16 +32,9 @@ const Dropdown = ({ options, onSelect }) => {
       <div className="dropdown-toggle" onClick={() => setIsOpen(!isOpen)}>
       
         <div className="dropdown-toggle-text">{selectedOption || (options.length > 0 && options[0].label) || "Select an option"}</div>
-        <div
-          className="material-symbols-outlined"
-          style={{
+        <MdOutlineExpandMore style={{
             transform: `rotate(${isOpen ? 180 : 0}deg)`,
-            transition: "all 0.25s",
-          
-          }}
-        >
-          expand_more
-        </div>
+            transition: "all 0.25s",}} />
         
       </div>
       {isOpen && (
