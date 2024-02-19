82c1bc05f2509d3a79f60331a2fd9ab360498e1b
Ahmed Khaled <80486237+Ahmed-Khaled24@users.noreply.github.com>
2024-02-19 12:29:10 +0200

    style: format the code

--- src/components/search-bar/search.component.jsx
+++ src/components/search-bar/search.component.jsx
@@ -1,30 +1,39 @@
-import { useState } from 'react';
-import searchlogo from '../../assets/search.svg';
-import "./search.style.css"
+import { useState } from "react";
+import searchLogo from "../../assets/search.svg";
+import "./search.style.css";
 
-const SearchBar = ({searchinput,setSearchInput}) => {
+const SearchBar = ({ searchInput, setSearchInput }) => {
+    const [isEntered, setEntered] = useState(false);
 
-    const [isEntered, setEntered]=useState(false);
-
-    const HandleChange =(value) =>{
-      setSearchInput(value);
+    const HandleChange = (value) => {
+        setSearchInput(value);
     };
 
-      const EnterEvent = (event) => {
-      if(event.keyCode === 13){
-        setEntered(current => !current);
-        setTimeout(function(){setEntered(current => !current);
-      },400);
-    };  
+    const EnterEvent = (event) => {
+        if (event.keyCode === 13) {
+            setEntered((current) => !current);
+            setTimeout(function () {
+                setEntered((current) => !current);
+            }, 400);
+        }
     };
-  
 
-  return (
-     <div className='search-bar-container'>
-     <img src={searchlogo} className="logo-search" alt="search logo" />
-     <input type='text' id="Text" className={isEntered? "entered-text text-search":"text-search"} value={searchinput} placeholder="Search" onChange={(e)=> HandleChange(e.target.value)} onKeyDown={(e) => EnterEvent(e)}/>
-     </div>
-  )
-}
+    return (
+        <div className="search-bar-container">
+            <img src={searchLogo} className="logo-search" alt="search logo" />
+            <input
+                type="text"
+                id="Text"
+                className={
+                    isEntered ? "entered-text text-search" : "text-search"
+                }
+                value={searchInput}
+                placeholder="Search"
+                onChange={(e) => HandleChange(e.target.value)}
+                onKeyDown={(e) => EnterEvent(e)}
+            />
+        </div>
+    );
+};
 
-export default SearchBar
+export default SearchBar;
