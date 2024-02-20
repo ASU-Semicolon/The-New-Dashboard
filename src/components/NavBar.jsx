7a940041601e6fc4bac17445dee1f0fa6d364245
mariam.khaled <mariam.khaled@rosenfieldhealth.com>
2024-02-20 15:09:01 +0200

    refactor: fix nav bar in mobile view
    change nav bar display from horizontal to vertical view after menu icon is pressed in mobile view
    
    Fixes issue #15

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -11,61 +11,58 @@ const Navbar = () => {
     setShowMenu(!showMenu);
   };
 
- return (
-   <header className="header">
-     <nav className="nav container">
-       <div className="nav__logo-container">
-         <NavLink to="/" className="nav__logo"> 
-           <img src={SemicolonLogo} className="nav__logo" alt="semicolon logo" />
-         </NavLink>
-       </div>
-       <div
-         className={`nav__menu ${showMenu ? 'show-menu' : ''}`}
-         id="nav-menu"
-       >
-         <ul className="nav__list">
-           <li className="nav__item">
-             <NavLink to="/users" className="nav__link">
-               Users
-             </NavLink>
-           </li>
-           <li className="nav__item">
-             <NavLink to="/committees" className="nav__link">
-               Committees
-             </NavLink>
-           </li>
-           <li className="nav__item">
-             <NavLink to="/workshops" className="nav__link">
-               Workshops
-             </NavLink>
-           </li>
-           <li className="nav__item">
-             <NavLink to="/participants" className="nav__link">
-               Participants
-             </NavLink>
-           </li>
-           <li className="nav__item">
-             <NavLink to="/members" className="nav__link">
-               Members
-             </NavLink>
-           </li>
-           <li className="nav__item">
-             <NavLink to="/interviewer" className="nav__link">
-               Interviewer
-             </NavLink>
-           </li>
-         </ul>
-         <div className="nav__close" id="nav-close" onClick={toggleMenu}>
-           <IoClose />
-         </div>
-       </div>
+  return (
+    <header className="header">
+      <nav className={`nav container ${showMenu ? 'show-menu' : ''}`}>
+        <div className="nav__logo-container">
+          <NavLink to="/" className="nav__logo"> 
+            <img src={SemicolonLogo} className="nav__logo" alt="semicolon logo" />
+          </NavLink>
+        </div>
+        <div className="nav__menu" id="nav-menu">
+          <ul className="nav__list">
+            <li className="nav__item">
+              <NavLink to="/users" className="nav__link">
+                Users
+              </NavLink>
+            </li>
+            <li className="nav__item">
+              <NavLink to="/committees" className="nav__link">
+                Committees
+              </NavLink>
+            </li>
+            <li className="nav__item">
+              <NavLink to="/workshops" className="nav__link">
+                Workshops
+              </NavLink>
+            </li>
+            <li className="nav__item">
+              <NavLink to="/participants" className="nav__link">
+                Participants
+              </NavLink>
+            </li>
+            <li className="nav__item">
+              <NavLink to="/members" className="nav__link">
+                Members
+              </NavLink>
+            </li>
+            <li className="nav__item">
+              <NavLink to="/interviewer" className="nav__link">
+                Interviewer
+              </NavLink>
+            </li>
+          </ul>
+          <div className="nav__close" id="nav-close" onClick={toggleMenu}>
+            <IoClose />
+          </div>
+        </div>
 
-       <div className="nav__toggle" id="nav-toggle" onClick={toggleMenu}>
-         <IoMenu />
-       </div>
-     </nav>
-   </header>
- );
+        <div className="nav__toggle" id="nav-toggle" onClick={toggleMenu}>
+          <IoMenu />
+        </div>
+      </nav>
+    </header>
+  );
 };
 
 export default Navbar;
