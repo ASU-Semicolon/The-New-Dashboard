966c59700d17299329d0aa8a1c854a669d1dd715
Zeyad Waleed Amin <ziadwalid20035@gmail.com>
2024-03-01 20:57:36 +0200

    fix: adjust searchBar
    fix the searchbar styling bugs and make it more reusable.
     add jsDocs comment.
      fix bugs and adjust logic of component.
      add two features submit by click on search or if timer finish

--- src/components/search-bar/search.component.jsx
+++ src/components/search-bar/search.component.jsx
@@ -1,36 +1,74 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import searchLogo from "../../assets/search.svg";
 import "./search.style.css";
-
-const SearchBar = ({ searchInput, setSearchInput }) => {
-    const [isEntered, setEntered] = useState(false);
-
-    const HandleChange = (value) => {
-        setSearchInput(value);
+/**
+ * SearchBar component for handling search input and submission.
+ *
+ * @component
+ * @param {Object} props - The component properties.
+ * @param {Function} props.setSearchInput - The function to set the search input value.
+ *
+ * @returns {JSX.Element} - The SearchBar component.
+ */
+const SearchBar = ({ setSearchInput = () => {} }) => {
+    const [isEntered, setIsEntered] = useState(false);
+    const [searchValue, setSearchValue] = useState("");
+    const changeHandler = (value) => {
+        setIsEntered(false);
+        setSearchValue(value);
     };
+    //submit search  after 1.5sec from last change of input value if it is not empty
+    useEffect(() => {
+        let timer;
+        if (searchValue.length > 0) {
+            timer = setTimeout(() => {
+                setIsEntered(true);
+                setSearchInput(searchValue);
+                setSearchValue("");
+            }, 1500);
+        }
+        return () => {
+            clearTimeout(timer);
+        };
+    }, [searchValue]);
 
-    const EnterEvent = (event) => {
-        if (event.keyCode === 13) {
-            setEntered((current) => !current);
-            setTimeout(function () {
-                setEntered((current) => !current);
-            }, 400);
+    //submit search by pressing search button
+    const submitHandler = () => {
+        setIsEntered(true);
+        setSearchInput(searchValue);
+        setSearchValue("");
+    };
+    //submit search by pressing enter key
+    const enterKeyHandler = (e) => {
+        if (e.key === "Enter") {
+            setIsEntered(true);
+            setSearchInput(searchValue);
+            setSearchValue("");
         }
     };
-
     return (
         <div className="search-bar-container">
-            <img src={searchLogo} className="logo-search" alt="search logo" />
+            <button
+                disabled={isEntered}
+                className="search-button"
+                onClick={submitHandler}
+            >
+                <img
+                    src={searchLogo}
+                    className="logo-search"
+                    alt="search logo"
+                />
+            </button>
             <input
                 type="text"
                 id="Text"
                 className={
                     isEntered ? "entered-text text-search" : "text-search"
                 }
-                value={searchInput}
+                value={searchValue}
                 placeholder="Search"
-                onChange={(e) => HandleChange(e.target.value)}
-                onKeyDown={(e) => EnterEvent(e)}
+                onChange={(e) => changeHandler(e.target.value)}
+                onKeyDown={enterKeyHandler}
             />
         </div>
     );
